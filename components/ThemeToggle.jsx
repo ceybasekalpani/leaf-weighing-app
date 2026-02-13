96cfe99c72767decdc8ed3c141c7e61bd37bfda3
ceybasekalpani <ceybasekalpani@gmail.com>
2026-02-13 14:21:25 +0530

    add ThemeToggle.jsx file✏️

---
+++ components/ThemeToggle.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { IconButton, useTheme } from 'react-native-paper';
+import { useTheme as useAppTheme } from '../src/context/ThemeContext';
+
+const ThemeToggle = () => {
+  const theme = useTheme();
+  const { isDarkMode, toggleTheme } = useAppTheme();
+
+  return (
+    <IconButton
+      icon={isDarkMode ? 'weather-sunny' : 'weather-night'}
+      iconColor={theme.colors.primary}
+      size={24}
+      onPress={toggleTheme}
+      style={{ marginRight: 8 }}
+    />
+  );
+};
+
+export default ThemeToggle;
